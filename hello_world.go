7269856cde7fdf94657d28786934e25d40a9ca65
Marianna Mao <mmao@fb.com>
2014-04-09 23:52:20 -0700

    Using Goji

--- hello_world.go
+++ hello_world.go
@@ -1,9 +1,18 @@
-/* This is a hello world */
-
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "net/http"
+
+  "github.com/zenazn/goji"
+  "github.com/zenazn/goji/web"
+)
+
+func hello(c web.C, w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello, %s!", c.UrlParams["name"])
+}
 
 func main() {
-    fmt.Println("hello world")
+  goji.Get("/hello/:name", hello)
+  goji.Serve()
 }
